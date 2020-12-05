a931d8015e66f8ebc04139500997a7a84608982d
Hugh Smalley <hsmalley@gmail.com>
2020-12-04 21:42:05 -0500

    Update download_datasets.sh
    Updated to the latest files I could find in that bucket

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -5,15 +5,15 @@ echo "by continuing execution of the script you are agreeing to the following da
 rm -rf "$OUT" 2>&1 /dev/null
 mkdir "$OUT"
 echo "downloading commercial use subset"
-wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-20/comm_use_subset.tar.gz
+wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-05-01/comm_use_subset.tar.gz
 echo "downloading non-commercial use subset"
-wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-20/noncomm_use_subset.tar.gz
+wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-05-01/noncomm_use_subset.tar.gz
 echo "downloading custom license subset"
-wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-20/custom_license.tar.gz
+wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-05-01/custom_license.tar.gz
 echo "downloading bioRxiv/medRxiv subset"
-wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-20/biorxiv_medrxiv.tar.gz
+wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-05-01/biorxiv_medrxiv.tar.gz
 echo "downloading metadata file"
-wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-20/metadata.csv
+wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-12-04/metadata.csv
 echo "downloading readme"
 wget --directory-prefix "$OUT" https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-03-13/all_sources_metadata_2020-03-13.readme
 echo "downloading document schema"
